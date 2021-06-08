5cc17e2ee0d713483fed5fe808e5b2f9ee845c61
Stevens <sdemervil@gmail.com>
2021-06-09 01:38:04 +0200

    add emailjs + form configuration

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,53 +1,75 @@
 <template>
-          <section class="section is-medium has-background-light">
-        <div class="container">
-          <div class="columns is-vcentered">
-            <div class="column">
-              <p :style="{fontSize: '25px', lineHeight: '50px'}">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore
-              </p>
-            </div>
-            <div class="column is-offset-2">
-              <form action="">
-                <div class="field">
-                  <div class="control">
-                    <input
-                      class="input is-success"
-                      type="text"
-                      placeholder="Prénom"
-                    />
-                  </div>
-                </div>
-                <div class="field">
-                  <div class="control">
-                    <input
-                      class="input is-success"
-                      type="email"
-                      placeholder="Adresse mail"
-                    />
-                  </div>
-                </div>
-
-                <div class="field">
-                  <div class="control">
-                    <button
-                      class="button is-medium px-6"
-                      :style="{fontWeight: '500'}"
-                    >
-                      JE M'INSCRIS
-                    </button>
-                  </div>
-                </div>
-              </form>
-            </div>
-          </div>
+  <section class="section is-medium has-background-light">
+    <div class="container">
+      <div class="columns is-vcentered">
+        <div class="column">
+          <p :style="{ fontSize: '25px', lineHeight: '50px' }">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore
+          </p>
         </div>
-      </section>
+        <div class="column is-offset-2">
+          <form @submit.prevent="sendEmail">
+            <div class="field">
+              <div class="control">
+                <input
+                  class="input is-success"
+                  type="text"
+                  name="user_name"
+                  placeholder="Prénom"
+                />
+              </div>
+            </div>
+            <div class="field">
+              <div class="control">
+                <input
+                  class="input is-success"
+                  type="email"
+                  name="user_email"
+                  placeholder="Adresse mail"
+                />
+              </div>
+            </div>
+
+            <div class="field">
+              <div class="control">
+                <input
+                  type="submit"
+                  value="JE M'INSCRIS"
+                  class="button is-medium px-6"
+                  :style="{ fontWeight: '500' }"
+                />
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
+import emailjs from "emailjs-com";
+
 export default {
-    
-}
+  methods: {
+    sendEmail: (e) => {
+      emailjs
+        .sendForm(
+          "service_rry823p",
+          "template_diw08fm",
+          e.target,
+          "user_Lg4YyWjub1DUlDW6Ur4jS"
+        )
+        .then(
+          (result) => {
+            alert("SUCCESS!", result.status, result.text);
+          },
+          (error) => {
+            alert("FAILED...", error);
+          }
+        );
+    },
+  },
+};
 </script>
