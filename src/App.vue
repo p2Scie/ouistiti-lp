fe974de1cc7aa3820db3d861e9f5c33c62da4cf1
Stevens <sdemervil@gmail.com>
2021-07-05 23:45:00 +0200

    remove chatbot + gtm + footer

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,17 @@
   <div :data-theme="isDarkModeON">
     <Header :isDarkModeON="isDarkModeON" :switchMode="switchMode"></Header>
     <router-view></router-view>
-    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import Header from "@/components/layout/Header.vue";
-import Footer from "@/components/layout/Footer.vue";
+// import Footer from "@/components/layout/Footer.vue";
 export default {
   name: "App",
   components: {
     Header,
-    Footer,
+    // Footer,
   },
   data() {
     return {
